9c4343a9bb541d7efefe59b8c41dc0084ebbd264
erickuo5124 <erickuo5124@gmail.com>
2021-11-19 08:59:57 +0800

    fix(Timebar): use correct width value in TimeBar component

--- Source/UI/TimeBar.cpp
+++ Source/UI/TimeBar.cpp
@@ -36,13 +36,22 @@ void TimeBar::drawLabels (Graphics& g)
 {
     const int headerWidth = editViewState.showHeaders ? 150 : 0;
     const int footerWidth = editViewState.showFooters ? 150 : 0;
-    const int interval = editViewState.timeToX(1, getWidth());
+    const int width = getWidth() - headerWidth - footerWidth;
+    const int interval = editViewState.timeToX(1, width);
     double scrollX = editViewState.scrollX.get();
-    double offset = editViewState.timeToX(scrollX, getWidth());
+    double offset = editViewState.timeToX(scrollX, width);
+    int zoomedInterval = interval;
+    while(zoomedInterval < 40) zoomedInterval *= 2;
     for (int xLabel = headerWidth + offset
          ; xLabel < getWidth() - footerWidth
-         ; xLabel += interval){
+         ; xLabel += zoomedInterval){
         g.setColour (Colours::white);
         g.drawRect (xLabel, 30, 1, 20);
+        int time = (xLabel - headerWidth - offset) / interval;
+        int sec = time % 60;
+        int min = time / 60;
+        g.drawText((min?String(min) + ":":"") + String(sec),
+                   xLabel, 0, zoomedInterval, 20,
+                   Justification::topLeft);
     }
 }
